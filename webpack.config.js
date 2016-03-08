c29e0c714b3316d390681547856754a174fc0585
Georgiy Rychko <rychko.georgiy@gmail.com>
2016-03-08 23:18:40 +0200

    fix(normalize tree rendering) - tree is rendered like a nested lists now

--- webpack.config.js
+++ webpack.config.js
@@ -2,47 +2,56 @@
 
 const path = require('path');
 const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+//const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 let config = {};
 
-config.devtool = 'sourcemap'
-config.context = __dirname
+config.devtool = 'sourcemap';
+config.context = __dirname;
 
 config.resolve = {
-  root: __dirname,
-  extensions: ['', '.ts', '.js', '.json']
-}
+    root: __dirname,
+    extensions: ['', '.ts', '.js', '.json']
+};
 
 config.entry = {
-  vendor: [
-    'zone.js/dist/zone-microtask',
-    'reflect-metadata',
-    'angular2/common',
-    'angular2/core',
-    'lodash'
-  ],
-  'app': ['./ng2-tree.ts', './demo/app.ts'],
+    vendor: [
+        'zone.js/dist/zone-microtask',
+        'reflect-metadata',
+        'angular2/common',
+        'angular2/core',
+        'lodash'
+    ],
+    'app': ['./ng2-tree.ts', './demo/app.ts'],
 };
 
 config.output = {
-  path: path.join(__dirname, './build'),
-  filename: '[name].js'
-}
+    path: path.join(__dirname, './build'),
+    filename: '[name].js'
+};
 
 config.module = {
-  loaders: [
-    {
-      test: /\.ts$/,
-      loader: 'ts-loader'
-    }
-  ]
-}
+    loaders: [
+        {
+            test: /\.ts$/,
+            loader: 'ts-loader'
+        },
+        {
+            test: /\.css$/,
+            loader: 'raw'
+        }
+    ]
+};
 
 config.plugins = [
-  new webpack.optimize.CommonsChunkPlugin({
-    name: 'common'
-  })
+    new CleanWebpackPlugin(['build'], {
+        root: __dirname,
+        verbose: true
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+        name: 'common'
+    })
 ];
 
 module.exports = config;
