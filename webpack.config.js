a9348ab4715b7eb9f52a731e390d37e0c8a20aae
Georgiy Rychko <rychko.georgiy@gmail.com>
2016-06-18 15:00:36 +0300

    update: new look for hovered nodes

--- webpack.config.js
+++ webpack.config.js
@@ -11,85 +11,85 @@ config.devtool = 'sourcemap';
 config.context = __dirname;
 
 config.resolve = {
-    root: __dirname,
-    extensions: ['', '.ts', '.js', '.json', '.styl', '.css', 'html']
+  root: __dirname,
+  extensions: ['', '.ts', '.js', '.json', '.styl', '.css', 'html']
 };
 
 config.entry = {
-    vendor: [
-        'zone.js',
-        'reflect-metadata',
-        '@angular/common',
-        '@angular/core',
-        '@angular/compiler',
-        '@angular/platform-browser',
-        '@angular/platform-browser-dynamic',
-        'lodash'
-    ],
-    'app': ['./ng2-branchy.ts', './demo/app.ts'],
+  vendor: [
+    'zone.js',
+    'reflect-metadata',
+    '@angular/common',
+    '@angular/core',
+    '@angular/compiler',
+    '@angular/platform-browser',
+    '@angular/platform-browser-dynamic',
+    'lodash'
+  ],
+  'app': ['./ng2-branchy.ts', './demo/app.ts']
 };
 
 config.output = {
-    path: path.join(__dirname, './build'),
-    publicPath: '/',
-    filename: '[name].js'
+  path: path.join(__dirname, './build'),
+  publicPath: '/',
+  filename: '[name].js'
 };
 
 config.module = {
-    loaders: [
-        {
-            test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
-            loader: "url?limit=10000&mimetype=application/font-woff"
-        }, {
-            test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
-            loader: "url?limit=10000&mimetype=application/font-woff"
-        }, {
-            test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-            loader: "url?limit=10000&mimetype=application/octet-stream"
-        }, {
-            test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-            loader: "file"
-        }, {
-            test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-            loader: "url?limit=10000&mimetype=image/svg+xml"
-        },
-        {
-            test: /\.ts$/,
-            loader: 'ts'
-        },
-        {
-            test: /\.css$/,
-            loader: 'style!css!stylus?resolve url'
-        },
-        {
-            test: /\.styl$/,
-            loader: 'to-string!css!stylus?resolve url'
-        },
-        {
-            test: /\.html$/,
-            loader: 'raw'
-        }
-    ]
+  loaders: [
+    {
+      test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+      loader: "url?limit=10000&mimetype=application/font-woff"
+    }, {
+      test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+      loader: "url?limit=10000&mimetype=application/font-woff"
+    }, {
+      test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+      loader: "url?limit=10000&mimetype=application/octet-stream"
+    }, {
+      test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+      loader: "file"
+    }, {
+      test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+      loader: "url?limit=10000&mimetype=image/svg+xml"
+    },
+    {
+      test: /\.ts$/,
+      loader: 'ts'
+    },
+    {
+      test: /\.css$/,
+      loader: 'style!css!stylus?resolve url'
+    },
+    {
+      test: /\.styl$/,
+      loader: 'to-string!css!stylus?resolve url'
+    },
+    {
+      test: /\.html$/,
+      loader: 'raw'
+    }
+  ]
 };
 
 config.plugins = [
-    new HtmlWebpackPlugin({
-        title: 'ng2-branchy',
-        template: 'demo/index.ejs',
-        inject: 'body'
-    }),
-    new CleanWebpackPlugin(['build'], {
-        root: __dirname,
-        verbose: true
-    }),
-    new webpack.optimize.CommonsChunkPlugin({
-        name: 'common'
-    })
+  new HtmlWebpackPlugin({
+    title: 'ng2-branchy',
+    template: 'demo/index.ejs',
+    inject: 'body'
+  }),
+  new CleanWebpackPlugin(['build'], {
+    root: __dirname,
+    verbose: true
+  }),
+  new webpack.optimize.CommonsChunkPlugin({
+    name: 'common'
+  })
 ];
 
 config.devServer = {
-    host: 'localhost',
-    port: 8080
+  host: 'localhost',
+  port: 8080
 };
 
 module.exports = config;
