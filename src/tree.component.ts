79542fd15168fb1a149da41d13efc15818bf5676
tkhalili <tkhalili@verint.com>
2017-09-25 18:01:44 +0300

    Exposing nodeChecked / Unchecked EventEmitters

--- src/tree.component.ts
+++ src/tree.component.ts
@@ -4,7 +4,7 @@ import {
 } from '@angular/core';
 import { TreeService } from './tree.service';
 import * as TreeTypes from './tree.types';
-import { NodeEvent } from './tree.events';
+import { NodeEvent, NodeCheckedEvent, NodeUncheckedEvent } from './tree.events';
 import { Tree } from './tree';
 import { TreeController } from './tree-controller';
 import { Subscription } from 'rxjs/Subscription';
@@ -45,6 +45,13 @@ export class TreeComponent implements OnInit, OnChanges, OnDestroy {
 
   @Output()
   public nodeCollapsed: EventEmitter<any> = new EventEmitter();
+
+  @Output()
+  public nodeChecked: EventEmitter<NodeCheckedEvent> = new EventEmitter();
+
+  @Output()
+  public nodeUnchecked: EventEmitter<NodeUncheckedEvent> = new EventEmitter();
+
 
   public tree: Tree;
   @ViewChild('rootComponent') public rootComponent;
@@ -90,6 +97,10 @@ export class TreeComponent implements OnInit, OnChanges, OnDestroy {
     this.subscriptions.push(this.treeService.nodeCollapsed$.subscribe((e: NodeEvent) => {
       this.nodeCollapsed.emit(e);
     }));
+
+    this.subscriptions.push(this.treeService.nodeChecked$.subscribe((e: NodeCheckedEvent) => this.nodeChecked.emit(e)));
+
+    this.subscriptions.push(this.treeService.nodeUnchecked$.subscribe((e: NodeUncheckedEvent) => this.nodeUnchecked.emit(e)));
   }
 
   public getController(): TreeController {
