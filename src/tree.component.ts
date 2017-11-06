709a81a99f56e57d0918f1eba15064b4fd93bd51
avadhoothaa <avadhoothachaitanya.audi@alacriti.com>
2017-11-06 13:00:21 +0530

    Build Tree Model and bind it two ways

--- src/tree.component.ts
+++ src/tree.component.ts
@@ -46,8 +46,12 @@ export class TreeComponent implements OnInit, OnChanges, OnDestroy {
   @Output()
   public nodeCollapsed: EventEmitter<any> = new EventEmitter();
 
-@Output()
-public loadNextLevel: EventEmitter<any> = new EventEmitter();
+  @Output()
+  public loadNextLevel: EventEmitter<any> = new EventEmitter();
+
+  @Output()
+  public treeChange: EventEmitter<any> = new EventEmitter();
+
 
   public tree: Tree;
   @ViewChild('rootComponent') public rootComponent;
@@ -70,14 +74,17 @@ public loadNextLevel: EventEmitter<any> = new EventEmitter();
   public ngOnInit(): void {
     this.subscriptions.push(this.treeService.nodeRemoved$.subscribe((e: NodeEvent) => {
       this.nodeRemoved.emit(e);
+      this.emitTree();
     }));
 
     this.subscriptions.push(this.treeService.nodeRenamed$.subscribe((e: NodeEvent) => {
       this.nodeRenamed.emit(e);
+      this.emitTree();
     }));
 
     this.subscriptions.push(this.treeService.nodeCreated$.subscribe((e: NodeEvent) => {
       this.nodeCreated.emit(e);
+      this.emitTree();
     }));
 
     this.subscriptions.push(this.treeService.nodeSelected$.subscribe((e: NodeEvent) => {
@@ -86,6 +93,7 @@ public loadNextLevel: EventEmitter<any> = new EventEmitter();
 
     this.subscriptions.push(this.treeService.nodeMoved$.subscribe((e: NodeEvent) => {
       this.nodeMoved.emit(e);
+      this.emitTree();
     }));
 
     this.subscriptions.push(this.treeService.nodeExpanded$.subscribe((e: NodeEvent) => {
@@ -99,6 +107,28 @@ public loadNextLevel: EventEmitter<any> = new EventEmitter();
     this.subscriptions.push(this.treeService.loadNextLevel$.subscribe((e: NodeEvent) => {
       this.loadNextLevel.emit(e);
     }));
+  }
+
+  private emitTree() {
+    if (this.rootComponent && this.rootComponent['tree']) {
+      const treeModel = this.buildModelFromTree(this.rootComponent['tree']);
+      this.treeChange.emit(treeModel);
+    }
+  }
+
+  private buildModelFromTree(tree) {
+    const model: TreeTypes.TreeModel = {
+      value: '',
+      children: [],
+      settings: tree.node.settings
+    };
+    model.value = tree.node.value;
+    if (tree.children) {
+      tree.children.forEach(child => {
+        model.children.push(this.buildModelFromTree(child));
+      });
+    }
+    return model;
   }
 
   public getController(): TreeController {
