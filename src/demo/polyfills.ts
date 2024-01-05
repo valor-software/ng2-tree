d3e1784e811682b38f0d32625a9649dbee11166f
farissimran <107765711+farissimran@users.noreply.github.com>
2024-01-05 20:28:47 +0500

    Angular 12 - Phase 1

--- src/demo/polyfills.ts
+++ src/demo/polyfills.ts
@@ -35,7 +35,7 @@ import 'core-js/es7/reflect';
 /***************************************************************************************************
  * Zone JS is required by Angular itself.
  */
-import 'zone.js/dist/zone'; // Included with Angular CLI.
+import 'zone.js'; // Included with Angular CLI.
 // import 'core-js/es6/regexp';
 // import 'core-js/es6/map';
 // import 'core-js/es6/set';
