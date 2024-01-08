53339bc86cd589da23de851b081520ef288a9d1a
farissimran <107765711+farissimran@users.noreply.github.com>
2024-01-08 14:13:21 +0500

    Angular 14 -> 15

--- test/test.ts
+++ test/test.ts
@@ -13,7 +13,6 @@ import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@ang
 
 // Unfortunately there's no typing for the `__karma__` variable. Just declare it as any.
 declare var __karma__: any;
-declare var require: any;
 
 // Prevent Karma from running prematurely.
 __karma__.loaded = function() {};
@@ -22,9 +21,5 @@ __karma__.loaded = function() {};
 getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting(), {
   teardown: { destroyAfterEach: false }
 });
-// Then we find all the tests.
-const context = require.context('./', true, /\.spec\.ts$/);
-// And load the modules.
-context.keys().map(context);
 // Finally, start Karma to run the tests.
 __karma__.start();
